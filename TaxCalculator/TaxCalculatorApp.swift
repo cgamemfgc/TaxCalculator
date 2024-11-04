c7d1eac2aad5127826f9965bf70d2ba3710d7d90
cgamemfgc <cgamemfgc@gmail.com>
2024-11-04 16:48:27 +0900

    新規作成

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by Takeshi Sakamoto on 2024/11/02.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaxCalculatorTests/TaxCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorTests.swift
+//  TaxCalculatorTests
+//
+//  Created by Takeshi Sakamoto on 2024/11/02.
+//
+
+import Testing
+@testable import TaxCalculator
+
+struct TaxCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
