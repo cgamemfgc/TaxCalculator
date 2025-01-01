0d884d1349f75bb25b5062be70dc7fa58b9466a1
cgamemfgc <cgamemfgc@gmail.com>
2025-01-01 21:23:44 +0900

    改修

--- TaxCalculator/ContentView.swift
+++ TaxCalculator/ContentView.swift
@@ -11,18 +11,60 @@ struct ContentView: View {
     @State var inputText = ""
     @State var tax8 = 0.0
     @State var tax10 = 0.0
+    
+    private let numberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.groupingSeparator = ","
+        return formatter
+    }()
+    
+    private func formatNumber(_ value: String) -> String {
+        if let number = Int(value) {
+                    return numberFormatter.string(from: NSNumber(value: number)) ?? value
+                }
+        return value
+    }
+    
     var body: some View {
         VStack (spacing: 20){
-            TextField("価格を入力",text: $inputText).keyboardType(.numberPad)
+            Text("消費税計算アプリ")
+                .font(.largeTitle)
+                .padding(.top, 100)
+                .padding(.bottom, 100)
+            
+            TextField("価格を入力",text:$inputText)
+                .keyboardType(.numberPad)
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .padding(.horizontal,20)
+            
             Button("計算"){
-                tax8 = (Double(inputText) ?? 0) * 0.08
-                tax10 = (Double(inputText) ?? 0) * 0.1
+                let input = Double(inputText) ?? 0
+                tax8 = floor(input * 1.08)
+                tax10 = floor(input * 1.1)
             }
-            Text("価格：\(inputText)")
-            Text("消費税8%：\(tax8)")
-            Text("消費税10%：\(tax10)")
+                .bold()
+                .frame(width: 100, height: 50)
+                .foregroundColor(Color.white)
+                .background(Color.orange)
+                .cornerRadius(10)
+                .padding(10)
+            HStack(){
+                Text("金額　　　：")
+                Text("\(formatNumber(inputText))円")
+            }
+            HStack(){
+                Text("消費税10％：")
+                Text("\(Int(tax10))円")
+            }
+            Spacer()
         }
-        .padding()
+        
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // 画面全体を埋める
+        .background(Color.green.opacity(0.3)) // 薄いグリーンに設定
+        .edgesIgnoringSafeArea(.all) // セーフエリアを無視して背景を広げる
     }
 }
 
